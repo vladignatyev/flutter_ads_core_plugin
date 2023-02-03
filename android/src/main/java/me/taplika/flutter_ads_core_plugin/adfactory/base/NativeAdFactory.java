f8eca698c8d681c3af078028e0f94a2b27ac9ef9
Vladimir Ignatev <ya.na.pochte@gmail.com>
2023-02-04 00:10:37 +0100

    added null safety and disabled irrelevant linter errors

--- android/src/main/java/me/taplika/flutter_ads_core_plugin/adfactory/base/NativeAdFactory.java
+++ android/src/main/java/me/taplika/flutter_ads_core_plugin/adfactory/base/NativeAdFactory.java
@@ -1,19 +1,25 @@
 package me.taplika.flutter_ads_core_plugin.adfactory.base;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import java.util.Map;
 
 import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin;
+import me.taplika.flutter_ads_core_plugin.adfactory.CustomOptions;
 
 import com.google.android.gms.ads.nativead.NativeAd;
 import com.google.android.gms.ads.nativead.NativeAdView;
 
 
 abstract public class NativeAdFactory implements GoogleMobileAdsPlugin.NativeAdFactory {
+    final private String TAG = "NativeAdFactory";
 
-    private String TAG = "NativeAdFactory";
     private Context context;
+
     public void setContext(Context context) {
         this.context = context;
     }
@@ -24,19 +30,36 @@ abstract public class NativeAdFactory implements GoogleMobileAdsPlugin.NativeAdF
         this.layoutId = layoutId;
     }
 
-    abstract protected NativeAdView inflate(Context context, Map<String, Object> customOptions, NativeAdContent adContent);
-    abstract protected void bindView(NativeAdView nativeAdView, Map<String, Object> customOptions, NativeAdContent adContent);
+    @SuppressWarnings("unused")
+    protected @NonNull NativeAdView inflate(@NonNull Context context,
+                                            @NonNull NativeAdContent adContent) {
+        return (NativeAdView) LayoutInflater.from(context).inflate(layoutId, null);
+    }
 
+    abstract protected void bindView(@NonNull NativeAdView nativeAdView,
+                                     @NonNull NativeAdContent adContent);
+
+    protected void applyCustomOptions(@NonNull View inflatedView,
+                                      @NonNull CustomOptions customOptions) {
+        customOptions.applyToView(inflatedView);
+    }
+
+    @SuppressWarnings("unused")
     @Override
-    public NativeAdView createNativeAd(
-            NativeAd nativeAd, Map<String, Object> customOptions) {
+    public NativeAdView createNativeAd(NativeAd nativeAd, Map<String, Object> customOptionsMap) {
         if (context == null) {
             throw new IllegalStateException("createNativeAd() called on NativeAdFactory but context is not bound.");
         }
+
         NativeAdContent adContent = new NativeAdContent(nativeAd);
 
-        NativeAdView nativeAdView = inflate(context, customOptions, adContent);
-        bindView(nativeAdView, customOptions, adContent);
+        NativeAdView nativeAdView = inflate(context, adContent);
+
+        applyCustomOptions(nativeAdView,
+                CustomOptions.fromHashMap(customOptionsMap));
+
+        bindView(nativeAdView, adContent);
+
         nativeAdView.setNativeAd(nativeAd);
 
         return nativeAdView;
