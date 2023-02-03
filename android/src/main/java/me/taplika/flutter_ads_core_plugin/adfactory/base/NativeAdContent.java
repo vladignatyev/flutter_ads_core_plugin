12fb389ba5dc2feb130514bfb07867ff9bba41cf
Vladimir Ignatev <ya.na.pochte@gmail.com>
2023-02-03 23:43:54 +0100

    added null safety annotations and conversion of the star rating to float

--- android/src/main/java/me/taplika/flutter_ads_core_plugin/adfactory/base/NativeAdContent.java
+++ android/src/main/java/me/taplika/flutter_ads_core_plugin/adfactory/base/NativeAdContent.java
@@ -1,19 +1,21 @@
 package me.taplika.flutter_ads_core_plugin.adfactory.base;
 
+import androidx.annotation.Nullable;
+
 import com.google.android.gms.ads.nativead.NativeAd;
 import com.google.android.gms.ads.MediaContent;
 
 public final class NativeAdContent {
-    public String store;
-    public String advertiser;
-    public String headline;
-    public String body;
-    public String cta;
-    public String price;
-    public Double starRating;
-    public NativeAd.Image icon;
+    public @Nullable String store;
+    public @Nullable String advertiser;
+    public @Nullable String headline;
+    public @Nullable String body;
+    public @Nullable String cta;
+    public @Nullable String price;
+    public @Nullable Float starRating;
+    public @Nullable NativeAd.Image icon;
 
-    public MediaContent mediaContent;
+    public @Nullable MediaContent mediaContent;
 
     public NativeAdContent(NativeAd nativeAd) {
         store = nativeAd.getStore();
@@ -22,7 +24,10 @@ public final class NativeAdContent {
         body = nativeAd.getBody();
         cta = nativeAd.getCallToAction();
         price = nativeAd.getPrice();
-        starRating = nativeAd.getStarRating();
+
+        if (nativeAd.getStarRating() != null)
+            starRating = nativeAd.getStarRating().floatValue();
+
         icon = nativeAd.getIcon();
         mediaContent = nativeAd.getMediaContent();
     }
